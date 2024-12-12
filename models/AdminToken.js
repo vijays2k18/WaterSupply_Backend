f9b6525dd2e20bac3c3992a6efa821c7614c95b1
Vijay S <84850388+vijays2k18@users.noreply.github.com>
2024-12-12 16:10:40 +0530

    Update AdminToken.js

--- models/AdminToken.js
+++ models/AdminToken.js
@@ -1,34 +1,40 @@
-// models/AdminToken.js
-import { Sequelize, DataTypes } from 'sequelize';
+import { Sequelize, DataTypes, Model } from 'sequelize';
 import sequelize from '../middleware/db.js';
 
-// Define the AdminToken model
-const AdminToken = sequelize.define('AdminToken', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'users', // The name of the users table
-      key: 'id', // The primary key of the users table
+// Define the AdminToken model using class-based syntax
+class AdminToken extends Model {}
+
+AdminToken.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    onDelete: 'CASCADE', // Delete the admin token if the user is deleted
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users', // The name of the users table
+        key: 'id', // The primary key of the users table
+      },
+      onDelete: 'CASCADE', // Delete the admin token if the user is deleted
+    },
+    admin_token: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  admin_token: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-}, {
-  tableName: 'AdminToken', // The name of the table in your database
-  timestamps: false, // Disable createdAt and updatedAt columns
-});
+  {
+    sequelize, // passing the sequelize instance
+    modelName: 'AdminToken', // Model name
+    tableName: 'AdminToken', // The name of the table in your database
+    timestamps: false, // Disable createdAt and updatedAt columns
+  }
+);
 
 // Association with Users model (to be defined in your users model)
-AdminToken.associate = function(models) {
+AdminToken.associate = function (models) {
   // Define the relationship between AdminToken and User
   AdminToken.belongsTo(models.User, { foreignKey: 'user_id' });
 };
